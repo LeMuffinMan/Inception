ad590d03735d011deca6134b605b06dad488a7a7
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2026-02-21 13:16:42 -0500

    checking port, logs, running and health ok

--- srcs/check_inception.sh
+++ srcs/check_inception.sh
@@ -9,17 +9,25 @@ echo
 CONTAINERS=$(docker compose ps)
 
 echo "Mariadb container: "
-MARIADB_CONTAINER=$(grep -q "mariadb" $CONTAINERS)
-if $MARIADB_CONTAINER; then
-    echo "running: ${GREEN}OK${NC}"
-    if grep "mariadb" $CONTAINERS | grep -q "unhealthy"; then
-        echo "healthy: ${RED}KO${NC}"
-    else
-        echo "healthy: ${GREEN}OK${NC}"
+if echo "$CONTAINERS" | grep "mariadb" > /dev/null && echo "$CONTAINERS" | grep "Up" > /dev/null && ! echo "$CONTAINERS" | grep "Restarting"; then
+    echo -e "   running: ${GREEN}OK${NC}"
+    if  ! grep "mariadb" $CONTAINERS 2>/dev/null | grep "mariadb" | grep -q "unhealthy"; then
+        echo -e "   healthy: ${GREEN}OK${NC}"
+    fi
+    if docker logs mariadb 2>&1 | grep "ready for connections" > /dev/null; then
+        echo -e "   logs: ${GREEN}OK${NC}"
+    fi
+    if [ "$(docker exec mariadb mariadb -u root -p\"${MYSQL_ROOT_PASSWORD}\" --connect-timeout=2 -e 'SELECT 1')" -eq 1 ]; then
+        echo -e "   probe: ${GREEN}OK${NC}"
+    fi
+    PORTS=$(docker port mariadb)
+    if [ "$(echo "$PORTS" | grep -c '3306/tcp')" -eq 2 ] && [ "$(echo "$PORTS" | wc -l)" -eq 2 ]; then
+        echo -e "   port: ${GREEN}OK${NC}"
     fi
 else
-    echo "running: ${RED}KO${NC}"
+    echo -e "   running: ${RED}KO${NC}"
+    echo -e "   logs: ${RED}KO${NC}"
+    echo -e "   healthy: ${RED}KO${NC}"
+    echo -e "   port: ${RED}KO${NC}"
+    echo -e "   probe: ${RED}KO${NC}"
 fi
-
-#cheker le port
-#faire une commande test a mariadb ?
