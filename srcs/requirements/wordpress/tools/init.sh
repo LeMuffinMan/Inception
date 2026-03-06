10f9bd9ff6cd17f31782c5cf72c2369f30791e93
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2026-03-06 13:33:01 -0500

    blueprint for wp container

---
+++ srcs/requirements/wordpress/tools/init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Recuperer les variables d'environnement
+
+cd /var/www/html
+
+if [ ! -f /var/www/html/wp-config.php ]; then
+# curl une config
+# la modifier et la configurer avec les var d'env
+#
+# Salts ?
+#
+# Perm on volumes
+#
+# installation of the website
+#
+# Gerer redis ici
+#
+# Encore les perms ?
+#
+#
+    echo "Wordpress successfully installed"
+else
+    echo "Wordpress already installed and configured"
+fi
+
+
+#setup port on socket
+
+# start wordpress with php
