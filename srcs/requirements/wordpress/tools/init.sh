aa7cfc6b73584600c2581d34b81ce298fc8ad0ee
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2026-03-11 09:52:12 -0400

    configuring wordpress, init.sh partly done

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,10 +1,51 @@
 #!/bin/sh
 
-# Recuperer les variables d'environnement
+set -e
+
+# checker docker secrets
+# export WORDPRESS_ADMIN_USER=$(cat /run/secrets/wordpress_admin_user)
+# export WORDPRESS_ADMIN_PASSWORD=$(cat /run/secrets/wordpress_admin_password)
+# export WORDPRESS_USER=$(cat /run/secrets/wordpress_user)
+# export WORDPRESS_PASSWORD=$(cat /run/secrets/wordpress_password)
+# export MYSQL_USER=$(cat /run/secrets/mysql_user)
+# export MYSQL_PASSWORD=$(cat /run/secrets/mysql_password)
+# export WORDPRESS_EMAIL=$(cat /run/secrets/wordpress_email)
 
 cd /var/www/html
 
+echo "Waiting for MariaDB..."
+until nc -z mariadb 3306; do
+  sleep 2
+done
+echo "MariaDB is ready"
+
 if [ ! -f /var/www/html/wp-config.php ]; then
+
+    wp core download --allow-root
+
+    wp config create \
+      --dbname=wordpress \
+      --dbuser=wpuser \
+      --dbpass=password \
+      --dbhost=mariadb:3306 \
+      --allow-root
+
+    wp core install \
+        --url="https://example.com" \
+        --title="My WordPress" \
+        --admin_user="admin" \
+        --admin_password="adminpass" \
+        --admin_email="admin@example.com" \
+        --skip-email \
+        --allow-root
+
+    wp user create \
+        user \
+        user@example.com \
+        --role=author \
+        --user_pass=userpass \
+        --allow-root
+
 # curl une config
 # la modifier et la configurer avec les var d'env
 #
@@ -28,5 +69,7 @@ fi
 #setup port on socket
 
 # start wordpress with php
+
+# chown -R www-data:www-data /var/www/html
 
 exec php-fpm83 --nodaemonize --fpm-config /etc/php83/php-fpm.conf
