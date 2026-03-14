d380170ee546e7b738e655d96b02e53b672017e3
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2026-03-14 10:30:52 -0400

    /etc/hosts updated with domain name

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -20,6 +20,8 @@ if [ ! -f /etc/nginx/ssl/cert.pem ]; then
 
 fi
 
+sed -i "s/server_name localhost/server_name ${DOMAIN_NAME}/g" /etc/nginx/http.d/nginx.conf
+
 # Docker monitors PID 1 : we don't want nginx to run as daemon (fork and exit)
 # thus, Docker sees nginx as PID 1 running in the container, it can forward signals and monitor it
 exec nginx -g "daemon off;"
