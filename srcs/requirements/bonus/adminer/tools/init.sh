9d1aa642c6ee3ef6f4539a29d200f15eb14ddea0
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2026-03-21 14:03:09 -0400

    configuring adminer interface access

--- srcs/requirements/bonus/adminer/tools/init.sh
+++ srcs/requirements/bonus/adminer/tools/init.sh
@@ -6,7 +6,7 @@ echo "Creating adminer folder at /var/www/html/adminer ..."
 mkdir -p /var/www/html/adminer || echo "Failed to create adminer folder"
 
 echo "Downloading adminer ..."
-curl -fsSl -o /var/www/html/adminer.php https://www.adminer.org/latest.php || echo "Failed to download adminer"
+curl -fsSl -o /var/www/html/index.php https://www.adminer.org/latest.php || echo "Failed to download adminer"
 
 echo "Adminer starting ..."
 # a cause de nginx et du TLS ca ne marchera pas si on passe pas avec le server php builtin
