b240e4deb4c9fd90239e95eef596a9110b4e6b08
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2026-03-21 15:34:33 -0400

    /usr/local/bin/init.sh: line 9: useradd: command not found

--- srcs/requirements/bonus/vsftpd/tools/init.sh
+++ srcs/requirements/bonus/vsftpd/tools/init.sh
@@ -1,17 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 FTP_USER=$(cat /run/secrets/ftp_user)
 FTP_PASS=$(cat /run/secrets/ftp_pass)
 
-useradd -m -d "$FTP_USER" || echo "Failed to create $FTP_USER"
-echo "$FTP_USER:$FTP_PASS" | chpasswd
+if ! id $FTP_USER  > /dev/null; then
+    useradd -m -d "$FTP_USER" || echo "Failed to create $FTP_USER"
+    echo "$FTP_USER:$FTP_PASS" | chpasswd
+fi
 
 mkdir -p /var/www/html
 #chown le repertoire ?
 
 mkdir -p /var/run/vsftpd/empty
-
 
 exec vsftpd /etc/vsftpd.conf
