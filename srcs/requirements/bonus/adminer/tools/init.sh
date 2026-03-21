618ad00c4b717629130fe117d5833f8cf4c20a0a
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2026-03-21 11:46:35 -0400

    adminer configured

--- srcs/requirements/bonus/adminer/tools/init.sh
+++ srcs/requirements/bonus/adminer/tools/init.sh
@@ -8,12 +8,6 @@ mkdir -p /var/www/html/adminer || echo "Failed to create adminer folder"
 echo "Downloading adminer ..."
 curl -fsSl -o /var/www/html/adminer.php https://www.adminer.org/latest.php || echo "Failed to download adminer"
 
-chown -R $(whoami):$(whoami) /var/www/html/adminer
-
-if [ -f /etc/php83/php-fpm.d/www.conf ]; then
-    echo "binding socket to listen on any interface on port 8080 ..."
-    sed -i 's/^listen =  .*/listen = 0.0.0.0:8080/' /etc/php83/php-fpm.d/www.conf || echo "Failed to edit /etc/php83/php-fpm/www.conf"
-fi
-
 echo "Adminer starting ..."
-exec php-fpm83 -F
+# a cause de nginx et du TLS ca ne marchera pas si on passe pas avec le server php builtin
+exec php83 -S 0.0.0.0:8080 -t /var/www/html
