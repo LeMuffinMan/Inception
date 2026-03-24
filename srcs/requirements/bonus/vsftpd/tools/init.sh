73f64433ed0979cf5834f77983b15001f0d97951
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2026-03-24 09:49:54 -0400

    vsftpd running healthy

--- srcs/requirements/bonus/vsftpd/tools/init.sh
+++ srcs/requirements/bonus/vsftpd/tools/init.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
-set -e
-
 FTP_USER=$(cat /run/secrets/ftp_user)
 FTP_PASS=$(cat /run/secrets/ftp_pass)
 
-if ! id $FTP_USER  > /dev/null; then
+if ! id $FTP_USER  > /dev/null 2>&1; then
     echo "Setting FTP_USER ..."
     adduser -D -h "/home/$FTP_USER" "$FTP_USER"
-    echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
+    echo "$FTP_USER:$FTP_PASS" | chpasswd
 else
-    echo "vsftpd is alrady configured"
+    echo "vsftpd is already configured"
 fi
 
-mkdir -p /var/run/vsftpd/empty
-#chown le repertoire ?
+# mkdir -p /var/run/vsftpd/empty
+
+cat > /etc/vsftpd/vsftpd.conf << 'EOF'
+listen=YES
+listen_ipv6=NO
+anonymous_enable=NO
+local_enable=YES
+write_enable=YES
+local_umask=022
+dirmessage_enable=YES
+use_localtime=YES
+xferlog_enable=YES
+connect_from_port_20=YES
+chroot_local_user=YES
+allow_writeable_chroot=YES
+secure_chroot_dir=/var/run/vsftpd/empty
+pasv_enable=YES
+pasv_min_port=21100
+pasv_max_port=21110
+EOF
 
 echo "Starting vsftpd ..."
-exec vsftpd /etc/vsftd/vsftpd.conf || echo "Failed to start vsftpd"
+exec vsftpd /etc/vsftpd/vsftpd.conf || echo "Failed to start vsftpd"
