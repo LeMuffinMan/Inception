84262627fbc84000cbf9ee2a5db88a5a9c763f47
muffin <162384047+LeMuffinMan@users.noreply.github.com>
2026-03-27 22:22:46 +0100

    Refactor entrypoint.sh for ChessGame deployment
    Updated sed commands to modify paths in index.html and removed obsolete script tag.

--- srcs/requirements/bonus/chessgame/entrypoint.sh
+++ srcs/requirements/bonus/chessgame/entrypoint.sh
@@ -10,11 +10,15 @@ URL=$(curl -s https://api.github.com/repos/LeMuffinMan/ChessGame/releases/latest
 curl -L "$URL" -o /tmp/chessgame.zip
 unzip -o /tmp/chessgame.zip -d /tmp/chessgame_extracted
 
-# trunk génère un sous-dossier dist/
 cp -r /tmp/chessgame_extracted/dist/. "$DEST/"
 
-# sed -i 's/ integrity="[^"]*"//g' "$DEST/index.html"
-# sed -i 's/ crossorigin="[^"]*"//g' "$DEST/index.html"
-# sed -i 's/.\/pkg\/chessgame.js/.\/chess_game-ca06d866cbbc2bd8.js\/g' "$DEST/index.html"
+
+sed -i \
+  -e 's|href="/chess_game-|href="./chess_game-|g' \
+  -e 's|from '"'"'/chess_game-|from '"'"'./chess_game-|g' \
+  -e 's|module_or_path: '"'"'/chess_game-|module_or_path: '"'"'./chess_game-|g' \
+  "$DEST/index.html"
+
+sed -i 's|<script type="module">[^<]*import init from "./pkg/chessgame.js"[^<]*</script>||' "$DEST/index.html"
 
 echo "ChessGame deployed to $DEST"
